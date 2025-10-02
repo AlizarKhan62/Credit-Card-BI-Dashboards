17667c1fbf6f17d68108c8302e25bcf7e266b11d
AlizarKhan62 <qasimsial51214@gmail.com>
2025-10-02 12:28:00 +0500

    Initial commit: BI dashboard project

---
+++ SQL Query - Financial Dashboard Data.sql
@@ -0,0 +1,13 @@
+-- SET datestyle = 'DMY';
+
+-- COPY cc_detail
+-- FROM 'D:/Credit Card BI Project/credit_card.csv'
+-- DELIMITER ','
+-- CSV HEADER;
+-- SELECT * FROM cc_detail
+-- SELECT * FROM cust_detail
+
+-- COPY cust_detail
+-- FROM 'D:/Credit Card BI Project/customer.csv'
+-- DELIMITER ','
+-- CSV HEADER;
